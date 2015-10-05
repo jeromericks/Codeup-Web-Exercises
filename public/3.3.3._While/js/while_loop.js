ad6be6f3255b894cdc93e49c8b93b749293b1d6f
jeromericks <jeromericks@gmail.com>
2015-10-05 10:10:15 -0500

    Added Math.pow

--- public/3.3.3._While/js/while_loop.js
+++ public/3.3.3._While/js/while_loop.js
@@ -4,9 +4,20 @@
 	var i = 2;
 	var body = document.getElementsByTagName('body')[0];
 
-	while (i <= 65536){
-		console.log(i);
-		body.innerHTML += "<p>" + i + "</p>";
-		i *= 2;
-	}
+	// while (i <= 65536){
+	// 	console.log(i);
+	// 	body.innerHTML += ("<p>" + i + "</p>");
+	// 	i *= 2;
+
+	// }
+
+	var i = 1;
+
+
+	do{
+		console.log(Math.pow(2, i));
+		body.innerHTML += ("<p>" + Math.pow(2, i) + "</p>");
+		i++;
+	} while (i <= 16);
+
 }());
