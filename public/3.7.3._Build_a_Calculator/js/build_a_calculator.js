ee83cf29645a047ef01debae914d2f39b74893dd
jeromericks <jeromericks@gmail.com>
2015-10-10 20:01:52 -0500

    Added practice files

--- public/3.7.3._Build_a_Calculator/js/build_a_calculator.js
+++ public/3.7.3._Build_a_Calculator/js/build_a_calculator.js
@@ -1,10 +1,279 @@
 (function () {
 	"use strict";
 
-	document.getElementById('clear').addEventListener('click', clear, false);
-	function clear(){
-		document.getElementById('text1').innerHTML = "0";
-		document.getElementById('text3').innerHTML = "0";
+	var textOne = document.getElementById('textOne');
+	var textTwo = document.getElementById('textTwo');
+	var textThree = document.getElementById('textThree');
+	var one = document.getElementById('one');
+	var two = document.getElementById('two');
+	var three = document.getElementById('three');
+	var four = document.getElementById('four');
+	var five = document.getElementById('five');
+	var six = document.getElementById('six');
+	var seven = document.getElementById('seven');
+	var eight = document.getElementById('eight');
+	var nine = document.getElementById('nine');
+	var zero = document.getElementById('zero');
+
+	// document.getElementById('clear').addEventListener('click', clear, false);
+	// function clear(){
+	// 	textOne.value = "00";
+	// 	textThree.value = "00";
+	// }
+
+	// var elementIsClicked = false; 
+	// function clickHandler(){ 
+ //  		elementIsClicked = true;
+	// }
+	// var element = document.getElementById('middle'); 
+	// element.addEventListener('click', clickHandler);
+
+
+	// var clearIsClicked = false; 
+	// function clearHandler(){ 
+ //  		clearIsClicked = true;
+ //  		elementIsClicked = false;
+	// }
+	// var clear = document.getElementById('clear'); 
+	// clear.addEventListener('click', clearHandler);
+
+
+	// var middle = document.getElementById('middle');
+	// middle.addEventListener('click', middle, false);
+	// middle = middle[0]
+	// function middle(){
+	// 	document.getElementById('textTwo').value = "+";
+	// }
+
+	// one.addEventListener('click', button1, false);
+	// function button1(){
+	// 	textOne.value = "1";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "1";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "1";
+	// 	}
+	// }
+
+	// two.addEventListener('click', button2, false);
+	// function button2(){
+	// 	textOne.value = "2";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "2";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "2";
+	// 	}
+	// }
+
+	// three.addEventListener('click', button3, false);
+	// function button3(){
+	// 	textOne.value = "3";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "3";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "3";
+	// 	}
+	// }
+
+	// four.addEventListener('click', button4, false);
+	// function button4(){
+	// 	textOne.value = "4";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "4";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "4";
+	// 	}
+	// }
+
+	// five.addEventListener('click', button5, false);
+	// function button5(){
+	// 	textOne.value = "5";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "5";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "5";
+	// 	}
+	// }
+
+	// six.addEventListener('click', button6, false);
+	// function button6(){
+	// 	textOne.value = "6";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "6";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "6";
+	// 	}
+	// }
+
+	// seven.addEventListener('click', button7, false);
+	// function button7(){
+	// 	textOne.value = "7";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "7";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "7";
+	// 	}
+	// }
+
+	// eight.addEventListener('click', button8, false);
+	// function button8(){
+	// 	textOne.value = "8";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "8";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "8";
+	// 	}
+	// }
+
+	// nine.addEventListener('click', button9, false);
+	// function button9(){
+	// 	textOne.value = "9";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "9";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "9";
+	// 	}
+	// }
+
+	// zero.addEventListener('click', button0, false);
+	// function button0(){
+	// 	textOne.value = "0";
+	// 	if(clearIsClicked){
+	// 		textOne.value = "0";
+	// 	}
+	// 	else if(elementIsClicked){
+	// 		textThree.value = "0";
+	// 	}
+	// }
+
+	function pushOne(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"1");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "1");
+		}
 	}
 
+	function pushTwo(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"2");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "2");
+		}
+	}
+
+	function pushThree(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"3");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "3");
+		}
+	}
+
+	function pushFour(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"4");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "4");
+		}
+	}
+
+	function pushFive(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"5");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "5");
+		}
+	}
+
+	function pushSix(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"6");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "6");
+		}
+	}
+
+	function pushSeven(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"7");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "7");
+		}
+	}
+
+	function pushEight(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"8");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "8");
+		}
+	}
+
+	function pushNine(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"9");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "9");
+		}
+	}
+
+	function pushZero(){
+		if (textOne.getAttribute('value')=='0'){
+			textOne.setAttribute('value',"0");
+		}
+		else {
+			textOne.setAttribute('value',textOne.getAttribute('value') + "0");
+		}
+	}
+
+	function pushAdd (){
+		if (textOne == textOne){
+			textOne = textThree;
+		}
+			textTwo.setAttribute('value','+');
+	}
+
+	function pushSubtract (){
+		if (textOne == textOne){
+			textOne = textThree;
+		}
+			textTwo.setAttribute('value','-');
+	}
+
+	function pushTimes (){
+		if (textOne == textOne){
+			textOne = textThree;
+		}
+			textTwo.setAttribute('value','*');
+	}
+
+	function pushDivide (){
+		if (textOne == textOne){
+			textOne = textThree;
+		}
+			textTwo.setAttribute('value','/');
+	}
+
+	
+
 }());
+
---
+++ public/states.js
@@ -0,0 +1,211 @@
+(function () {
+	"use strict";
+
+	var states = [{
+		state : 'Alabama', 
+		capital : 'Montgomery', 
+		bird : 'Yellowhammer'
+		},{
+		state : 'Alaska', 
+		capital : 'Juneau', 
+		bird : 'Willow Ptarmigan'
+		},{
+		state : 'Arizona', 
+		capital : 'Phoenix', 
+		bird : 'Cactus Wren'
+		},{
+		state : 'Arkansas', 
+		capital : 'Little Rock', 
+		bird : 'Mockingbird'
+		},{
+		state : 'California', 
+		capital : 'Sacramento', 
+		bird : 'California Valley Quail'
+		},{
+		state : 'Colorado', 
+		capital : 'Denver', 
+		bird : 'Lark Bunting'
+		},{
+		state : 'Connecticut', 
+		capital : 'Hartford', 
+		bird : 'Robin'
+		},{
+		state : 'Delaware', 
+		capital : 'Dover', 
+		bird : 'Blue Hen Chicken'
+		},{
+		state : 'Florida', 
+		capital : 'Tallahassee', 
+		bird : 'Mockingbird' 
+		},{
+		state : 'Georgia', 
+		capital : 'Atlanta', 
+		bird : 'Brown Thrasher'
+		},{
+		state : 'Hawaii', 
+		capital : 'Honolulu', 
+		bird : 'Nene'
+		},{
+		state : 'Idaho', 
+		capital : 'Boise', 
+		bird : 'Mountain Bluebird'
+		},{
+		state : 'Illinois', 
+		capital : 'Springfield', 
+		bird : 'Cardinal'
+		},{
+		state : 'Indiana', 
+		capital : 'Indianapolis', 
+		bird : 'Cardinal'
+		},{
+		state : 'Iowa', 
+		capital : 'Des Moines', 
+		bird : 'Eastern Goldfinch'
+		},{
+		state : 'Kansas', 
+		capital : 'Topeka', 
+		bird : 'Western Meadowlark'
+		},{
+		state : 'Kentucky', 
+		capital : 'Frankfort', 
+		bird : 'Cardinal'
+		},{
+		state : 'Louisiana', 
+		capital : 'Baton Rouge', 
+		bird : 'Eastern Brown Pelican'
+		},{
+		state : 'Maine', 
+		capital : 'Augusta', 
+		bird : 'Chickadee'
+		},{
+		state : 'Maryland', 
+		capital : 'Annapolis', 
+		bird : 'Baltimore Oriole'
+		},{
+		state : 'Massachusetts', 
+		capital : 'Boston', 
+		bird : 'Chickadee'
+		},{
+		state : 'Michigan', 
+		capital : 'Lansing', 
+		bird : 'Robin'
+		},{
+		state : 'Minnesota', 
+		capital : 'St. Paul', 
+		bird : 'Common Loon'
+		},{
+		state : 'Mississippi', 
+		capital : 'Jackson', 
+		bird : 'Mockingbird'
+		},{
+		state : 'Missouri', 
+		capital : 'Jefferson City', 
+		bird : 'Bluebird' 
+		},{
+		state : 'Montana', 
+		capital : 'Helena', 
+		bird : 'Western Meadowlark'
+		},{
+		state : 'Nebraska', 
+		capital : 'Lincoln', 
+		bird : 'Western Meadowlark'
+		},{
+		state : 'Nevada', 
+		capital : 'Carson City', 
+		bird : 'Mountain Bluebird'
+		},{
+		state : 'New Hampshire', 
+		capital : 'Concord', 
+		bird : 'Purple Finch'
+		},{
+		state : 'New Jersey', 
+		capital : 'Trenton', 
+		bird : 'Eastern Goldfinch'
+		},{
+		state : 'New Mexico', 
+		capital : 'Santa Fe', 
+		bird : 'Roadrunner'
+		},{
+		state : 'New York', 
+		capital : 'Albany', 
+		bird : 'Bluebird'
+		},{
+		state : 'North Carolina', 
+		capital : 'Raleigh', 
+		bird : 'Cardinal'
+		},{
+		state : 'North Dakota', 
+		capital : 'Bismarck', 
+		bird : 'Western Meadowlark'
+		},{
+		state : 'Ohio', 
+		capital : 'Columbus', 
+		bird : 'Cardinal'
+		},{
+		state : 'Oklahoma', 
+		capital : 'Oklahoma City', 
+		bird : 'Scissor-tailed Flycatcher'
+		},{
+		state : 'Oregon', 
+		capital : 'Salem', 
+		bird : 'Western Meadowlark'
+		},{
+		state : 'Pennsylvania', 
+		capital : 'Harrisburg', 
+		bird : 'Ruffed Grouse'
+		},{
+		state : 'Rhode Island', 
+		capital : 'Providence', 
+		bird : 'Rhode Island Red'
+		},{
+		state : 'South Carolina', 
+		capital : 'Columbia', 
+		bird : 'Great Carolina Wren'
+		},{
+		state : 'South Dakota', 
+		capital : 'Pierre', 
+		bird : 'Ring-necked Pheasant'
+		},{
+		state : 'Tennessee', 
+		capital : 'Nashville', 
+		bird : 'Mockingbird'
+		},{
+		state : 'Texas', 
+		capital : 'Austin', 
+		bird : 'Mockingbird'
+		},{
+		state : 'Utah', 
+		capital : 'Salt Lake City', 
+		bird : 'Common American Gull'
+		},{
+		state : 'Vermont', 
+		capital : 'Montpelier', 
+		bird : 'Hermit Thrush'
+		},{
+		state : 'Virginia', 
+		capital : 'Richmond', 
+		bird : 'Cardinal'
+		},{
+		state : 'Washington', 
+		capital : 'Olympia', 
+		bird : 'Willow Goldfinch'
+		},{
+		state : 'West Virginia', 
+		capital : 'Charleston', 
+		bird : 'Cardinal'
+		},{
+		state : 'Wisconsin', 
+		capital : 'Madison', 
+		bird : 'Robin'
+		},{
+		state : 'Wyoming', 
+		capital : 'Cheyennes', 
+		bird : 'Western Meadowlark'
+	}];
+
+	states.forEach(function (state, index, array) {
+		console.log("State: " + states.state);
+	});
+
+
+}());
