fa695d6249a9742f82d8ff21b3f71c870e5990c4
jeromericks <jeromericks@gmail.com>
2015-10-13 08:58:17 -0500

    Improved calculator

--- public/3.7.3._Build_a_Calculator/js/build_a_calculator.js
+++ public/3.7.3._Build_a_Calculator/js/build_a_calculator.js
@@ -1,170 +1,256 @@
 (function () {
 	"use strict";
 
-	var textOne = document.getElementById('textOne');
-	var textTwo = document.getElementById('textTwo');
-	var textThree = document.getElementById('textThree');
-	var textBoxSelector = textOne;
-	var one = document.getElementById('one');
-	var two = document.getElementById('two');
-	var three = document.getElementById('three');
-	var four = document.getElementById('four');
-	var five = document.getElementById('five');
-	var six = document.getElementById('six');
-	var seven = document.getElementById('seven');
-	var eight = document.getElementById('eight');
-	var nine = document.getElementById('nine');
-	var zero = document.getElementById('zero');
-	var plus = document.getElementById('plus');
-	var minus = document.getElementById('minus');
-	var times = document.getElementById('times');
-	var divide = document.getElementById('divide');
-	var equals = document.getElementById('equals');
-	var clear = document.getElementById('clear');
+	// var textOne = document.getElementById('textOne');
+	// var textTwo = document.getElementById('textTwo');
+	// var textThree = document.getElementById('textThree');
+	// var one = document.getElementById('one');
+	// var two = document.getElementById('two');
+	// var three = document.getElementById('three');
+	// var four = document.getElementById('four');
+	// var five = document.getElementById('five');
+	// var six = document.getElementById('six');
+	// var seven = document.getElementById('seven');
+	// var eight = document.getElementById('eight');
+	// var nine = document.getElementById('nine');
+	// var zero = document.getElementById('zero');
+	// var plus = document.getElementById('plus');
+	// var minus = document.getElementById('minus');
+	// var times = document.getElementById('times');
+	// var divide = document.getElementById('divide');
+	// var equals = document.getElementById('equals');
+	// var clear = document.getElementById('clear');
+	// var text = textOne;
 	
 
-	function pushOne(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"1");
+	// function One(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"1");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "1");
+	// 	}
+	// }
+	// function Two(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"2");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "2");
+	// 	}
+	// }
+	// function Three(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"3");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "3");
+	// 	}
+	// }	
+	// function Four(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"4");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "4");
+	// 	}
+	// }
+	// function Five(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"5");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "5");
+	// 	}
+	// }
+	// function Six(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"6");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "6");
+	// 	}
+	// }
+	// function Seven(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"7");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "7");
+	// 	}
+	// }
+	// function Eight(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"8");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "8");
+	// 	}
+	// }
+	// function Nine(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"9");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "9");
+	// 	}
+	// }
+	// function Zero(){
+	// 	if (text.getAttribute('value')=='0'){
+	// 		text.setAttribute('value',"0");
+	// 	}
+	// 	else {
+	// 		text.setAttribute('value',text.getAttribute('value') + "0");
+	// 	}
+	// }
+	// function Times (){
+	// 	if (text == textOne){
+	// 		text = textThree;
+	// 	}
+	// 		textTwo.setAttribute('value','*');
+	// }
+	// function Divide (){
+	// 	if (text == textOne){
+	// 		text = textThree;
+	// 	}
+	// 		textTwo.setAttribute('value','/');
+	// }
+	// function Plus (){
+	// 	if (text == textOne){
+	// 		text = textThree;
+	// 	}
+	// 		textTwo.setAttribute('value','+');
+	// }
+	// function Minus (){
+	// 	if (text == textOne){
+	// 		text = textThree;
+	// 	}
+	// 		textTwo.setAttribute('value','-');
+	// }
+	// function Clear(){
+	// 	text = textOne;
+	// 	textOne.setAttribute('value','0');
+	// 	textTwo.setAttribute('value','');
+	// 	textThree.setAttribute('value','0');
+	// }
+	// function Equals (){
+	// 	var a = textOne.getAttribute('value');
+	// 	var b = textTwo.getAttribute('value');
+	// 	var c = textThree.getAttribute('value');
+	// 	if(b == '*'){
+	// 		var ans = Number(a) * Number(c);
+	// 		textOne.setAttribute('value', '');
+	// 		textTwo.setAttribute('value',ans);
+	// 		textThree.setAttribute('value','0');
+	// 	}
+	// 	if(b == '/'){
+	// 		var ans = Number(a) / Number(c);
+	// 		textOne.setAttribute('value', '');
+	// 		textTwo.setAttribute('value',ans);
+	// 		textThree.setAttribute('value','0');
+	// 	}
+	// 	if(b == '-'){
+	// 		var ans = Number(a) - Number(c);
+	// 		textOne.setAttribute('value', '');
+	// 		textTwo.setAttribute('value',ans);
+	// 		textThree.setAttribute('value','');
+	// 	}
+	// 	if(b == '+'){
+	// 		var ans = Number(a) + Number(c);
+	// 		textOne.setAttribute('value', '');
+	// 		textTwo.setAttribute('value',ans);
+	// 		textThree.setAttribute('value','');
+	// 	}
+	// }
+
+	// one.addEventListener('click', One,false);
+	// two.addEventListener('click',Two,false);
+	// three.addEventListener('click',Three,false);
+	// four.addEventListener('click',Four,false);
+	// five.addEventListener('click',Five,false);
+	// six.addEventListener('click',Six,false);
+	// seven.addEventListener('click',Seven,false);
+	// eight.addEventListener('click',Eight,false);
+	// nine.addEventListener('click',Nine,false);
+	// zero.addEventListener('click',Zero,false);
+	// times.addEventListener('click', Times,false);
+	// plus.addEventListener('click',Plus,false);
+	// divide.addEventListener('click',Divide,false);
+	// minus.addEventListener('click',Minus,false);
+	// equals.addEventListener('click',Equals,false);
+	// clear.addEventListener('click', Clear,false);
+
+
+
+	var answer;
+	var buttons = document.getElementsByTagName('button');
+	var operators = document.getElementsByClassName('operator');
+	var left = document.getElementById('left');
+	var operator = document.getElementById('operator');
+	var right = document.getElementById('right');
+
+	//add event listeners to the buttons
+
+	function listener(event) {
+		var clickedButton = this;
+		if(this.getAttribute('class') == 'number') {
+			clickedButton = this.getAttribute('data-value');
+			left = clickedButton;
+			console.log(left);
+		} 
+		else if(isNaN(this.clickedButton)) {
+			clickedButton = this.getAttribute('data-value');
+			operator = clickedButton;
+			console.log(operator);
 		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "1");
+		else if(this.clickedButton === "=") {
+			 calculateOutput();
+		}
+		else {
+			Clear();
+		}
+
 	}
-	function pushTwo(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"2");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "2");
+
+	for(var i = 0; i < buttons.length; i += 1) {
+		buttons[i].addEventListener('click', listener, false);
 	}
-	function pushThree(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"3");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "3");
-	}	
-	function pushFour(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"4");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "4");
-	}
-	function pushFive(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"5");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "5");
-	}
-	function pushSix(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"6");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "6");
-	}
-	function pushSeven(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"7");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "7");
-	}
-	function pushEight(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"8");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "8");
-	}
-	function pushNine(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"9");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "9");
-	}
-	function pushZero(){
-		if (textBoxSelector.getAttribute('value')=='0'){
-			textBoxSelector.setAttribute('value',"0");
-		}
-		else
-			textBoxSelector.setAttribute('value',textBoxSelector.getAttribute('value') + "0");
-	}
-	function pushTimes (){
-		if (textBoxSelector == textOne){
-			textBoxSelector = textThree;
-		}
-			textTwo.setAttribute('value','*');
-	}
-	function pushDivide (){
-		if (textBoxSelector == textOne){
-			textBoxSelector = textThree;
-		}
-			textTwo.setAttribute('value','/');
-	}
-	function pushPlus (){
-		if (textBoxSelector == textOne){
-			textBoxSelector = textThree;
-		}
-			textTwo.setAttribute('value','+');
-	}
-	function pushMinus (){
-		if (textBoxSelector == textOne){
-			textBoxSelector = textThree;
-		}
-			textTwo.setAttribute('value','-');
-	}
-	function pushClear(){
-		textBoxSelector = textOne;
-		textOne.setAttribute('value','0');
-		textTwo.setAttribute('value','');
-		textThree.setAttribute('value','0');
-	}
-	function pushEquals (){
-		var b1 = textOne.getAttribute('value');
-		var b2 = textTwo.getAttribute('value');
-		var b3 = textThree.getAttribute('value');
-		if(b2 == '*'){
-			var ans = Number(b1) * Number(b3);
-			textOne.setAttribute('value',ans);
-			textThree.setAttribute('value','0');
-		}
-		if(b2 == '/'){
-			var ans = Number(b1) / Number(b3);
-			textOne.setAttribute('value',ans);
-			textThree.setAttribute('value','0');
-		}
-		if(b2 == '-'){
-			var ans = Number(b1) - Number(b3);
-			textOne.setAttribute('value',ans);
-			textThree.setAttribute('value','0');
-		}
-		if(b2 == '+'){
-			var ans = Number(b1) + Number(b3);
-			textOne.setAttribute('value',ans);
-			textThree.setAttribute('value','0');
+
+
+	function calcuateOutput() {
+		switch(operator){
+			case('*'):
+				answer = Number(left) * Number(right);
+				left.setAttribute('value', answer);
+				break;
+			case('/'):
+				answer = Number(left) / Number(right);
+				left.setAttribute('value', answer);
+				break;
+			case('-'):
+				answer = Number(left) - Number(right);
+				left.setAttribute('value', answer);
+				break;
+			case('+'):
+				answer = Number(left) + Number(right);
+				left.setAttribute('value', answer);
 		}
 	}
 
-	one.addEventListener('click', pushOne,false);
-	two.addEventListener('click',pushTwo,false);
-	three.addEventListener('click',pushThree,false);
-	four.addEventListener('click',pushFour,false);
-	five.addEventListener('click',pushFive,false);
-	six.addEventListener('click',pushSix,false);
-	seven.addEventListener('click',pushSeven,false);
-	eight.addEventListener('click',pushEight,false);
-	nine.addEventListener('click',pushNine,false);
-	zero.addEventListener('click',pushZero,false);
-	times.addEventListener('click', pushTimes,false);
-	plus.addEventListener('click',pushPlus,false);
-	divide.addEventListener('click',pushDivide,false);
-	minus.addEventListener('click',pushMinus,false);
-	equals.addEventListener('click',pushEquals,false);
-	clear.addEventListener('click', pushClear,false);
+	function Clear() {
+		left = left;
+		left.setAttribute('value','0');
+		operator.setAttribute('value','');
+		right.setAttribute('value','0');
+	}
+
+	function Operator () {
+		if (left == left) {
+			left = right;
+		}
+			operators.getAttribute('data-value');
+
+	}
 
 }());
 
