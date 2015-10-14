ed49444e0f23747050610f088332c41133612931
jeromericks <jeromericks@gmail.com>
2015-10-14 14:59:02 -0500

    Added Element Selector Assignment

---
+++ public/4.1.3._Element_Selector/js/jquery_exercises.js
@@ -0,0 +1,11 @@
+(function () {
+	"use strict";
+	
+	$(document).ready(function() {
+		$('li').css('font-size', '20px');
+		$('h1, p, li').css('background-color', 'yellow');
+
+		alert($('h1').html());
+	});
+
+}());
