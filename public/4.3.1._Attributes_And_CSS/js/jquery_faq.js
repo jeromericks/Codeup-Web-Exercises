e3907fbc69fec88b83c666b709d3389c056ae07f
jeromericks <jeromericks@gmail.com>
2015-10-15 13:28:52 -0500

    Added Attributes and CSS Assignment

---
+++ public/4.3.1._Attributes_And_CSS/js/jquery_faq.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	"use strict";
+
+    $('a').click(function(event) {
+        event.preventDefault();
+        $('dd').toggleClass('invisible');
+    });
+        
+
+});
+
+
+
+
+
