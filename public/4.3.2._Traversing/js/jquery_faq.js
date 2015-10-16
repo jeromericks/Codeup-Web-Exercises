01af3d0d878f154ec52b86246212803a72c31e34
jeromericks <jeromericks@gmail.com>
2015-10-16 13:35:52 -0500

    Added a new way to bold the first li's

--- public/4.3.2._Traversing/js/jquery_faq.js
+++ public/4.3.2._Traversing/js/jquery_faq.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 	"use strict";
 
-    $('ul li:first-child').css('font-weight', 'bold');
+    // $('ul li:first-child').css('font-weight', 'bold');
+
+    $("ul").each(function(){
+    	$(this).find("li").first().css('font-weight', 'bold');
+	});
 
     $('li').click(function(event){
 		$(this).parent().toggleClass('invisible');
