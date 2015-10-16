5e7546077e88f37ae2fceb522a07465616178215
jeromericks <jeromericks@gmail.com>
2015-10-16 11:57:41 -0500

    Improved Keyboard Events and Added fadeOut and fadeIn to Fading Assignment

--- public/4.2.2._Keyboard_Events/js/konami.js
+++ public/4.2.2._Keyboard_Events/js/konami.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
 	"use strict";
 
-
-	var body = document.getElementsByTagName('body')[0];
 	var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
 	var user = [];
 
-	var listener = function (event) {
+	$(document).keyup(function(event){
 		user.push(event.keyCode);
-		console.log(user);
-		if (konami.length == user.length) {
-			if (konami.toString() == user.toString()) {
-				alert("You have added 30 lives!");
-			};
-		};
-	};
 
-	body.addEventListener("keyup", listener, false);
+		if(user[user.length-1] != konami[user.length-1]) {
+			user = [];
+		}
+
+		if(user.length == konami.length) {
+			alert('You have added 30 Lives!')
+		}
+	});
+
 });
-
-
-
-
-
---
+++ public/4.4.2._Fading/js/jquery_faq.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	"use strict";
+
+    $('ul li:first-child').css('font-weight', 'bold');
+
+       
+   	$('a').click(function(event) {
+        event.preventDefault();
+        $('dd').fadeToggle('invisible');
+    });
+
+    $('h3').on("click", fadeOut);
+   
+    function fadeOut(event){
+        $(this).next().fadeOut();
+        $(this).off("click").on("click", fadeIn);
+    }
+    function fadeIn(event){
+        $(this).next().fadeIn();
+        $(this).off("click").on("click", fadeOut);
+    }    
+
+
+});
+
+
+
+
+
