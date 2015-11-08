e1172e456612b2e24dabbcdf52a241a42197999c
jeromericks <jeromericks@gmail.com>
2015-11-08 02:18:58 -0600

    Navigation bar works now, added glowing text, up arrow on smaller screens, typing text

--- public/1.4.5._Twitter_Bootstrap/index.js
+++ public/1.4.5._Twitter_Bootstrap/index.js
@@ -36,4 +36,67 @@ $(document).ready(function() {
 		}
 	});
 
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+	    $('.page-scroll a').bind('click', function(event) {
+	        var $anchor = $(this);
+	        $('html, body').stop().animate({
+	            scrollTop: $($anchor.attr('href')).offset().top
+	        }, 1500, 'easeInOutExpo');
+	        event.preventDefault();
+	    });
+	});
+
+	// Floating label headings for the contact form
+	$(function() {
+	    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+	        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+	    }).on("focus", ".floating-label-form-group", function() {
+	        $(this).addClass("floating-label-form-group-with-focus");
+	    }).on("blur", ".floating-label-form-group", function() {
+	        $(this).removeClass("floating-label-form-group-with-focus");
+	    });
+	});
+
+	var words = ['Jerome Ricks', "Full-Stack Web Developer", "Hi", "Bye"],
+    	div = document.getElementById('name'),
+    	character_counter = 0,
+    	counter = 0;
+
+	function updateWords(){ 
+		if(character_counter < words[counter].length) {
+		  if(words[counter][character_counter] == ' ') {
+		    div.innerHTML = div.innerHTML+'&nbsp;';
+		  }
+		  else {
+		    if(character_counter == 0){
+		    	div.innerHTML = words[counter][character_counter];
+		    } else {
+		    	div.innerHTML = div.innerHTML+ words[counter][character_counter];
+		    }
+		  }
+		}
+
+		character_counter++;
+
+		if(character_counter == words[counter].length + 4){  
+		    if(counter == 0){
+		        div = document.getElementById('desc');
+		        character_counter = 0;
+		        counter ++;
+		    } 
+	    }
+	}
+
+	var nameInterval = setInterval(updateWords, 300);
+
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+	    target: '.navbar-fixed-top'
+	})
+
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+	    $('.navbar-toggle:visible').click();
+	});
 });
